9d9dee83c9da8e0c1790873f8accd74704b54f53
João Henrique Victorino da Silva <Joo.Silva@salas.aulas>
2019-10-31 22:33:57 -0300

    melhoria de consumo

--- retry/retrySample.py
+++ retry/retrySample.py
@@ -1,10 +1,16 @@
 import requests
 from retry import retry
+from retry.api import retry_call
 
-@retry(tries=3)
+#annotation
+#@retry(tries=3)
 def do_request():
     response = requests.get("http://localhost:5000/")
     response.raise_for_status()
     print(response.text)
 
-do_request()
+#annotation
+#do_request()
+
+#retry call method
+retry_call(do_request, tries=4)
